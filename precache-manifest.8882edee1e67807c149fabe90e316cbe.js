6657b6f2f1d8011f5a1c8be24f0dd3e87712df81
christian <possidonio.ch@gmail.com>
2020-04-03 14:38:21 -0300

    Novo laylout

---
+++ precache-manifest.8882edee1e67807c149fabe90e316cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352df5c41b0d8fd25e7369adddd07e85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e2958dbd9b115303703",
+    "url": "/static/css/main.4e4b2793.chunk.css"
+  },
+  {
+    "revision": "0e399bf8656a147886d7",
+    "url": "/static/js/2.1dbd870f.chunk.js"
+  },
+  {
+    "revision": "9e2958dbd9b115303703",
+    "url": "/static/js/main.f5ca7fe5.chunk.js"
+  },
+  {
+    "revision": "425911a828ed1b6fbc25",
+    "url": "/static/js/runtime-main.a24c4e9f.js"
+  },
+  {
+    "revision": "3e4c7830da9fd12a49130206ca141738",
+    "url": "/static/media/profile.3e4c7830.png"
+  }
+]);
